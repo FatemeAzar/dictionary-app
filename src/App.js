f7d9fb23c5dd0025ef1ab45f0b0097b717bc0184
FatemeAazar <fatemeazarhazin91@gmail.com>
2022-02-27 22:57:47 +0330

    search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from "./logo.svg";
+// import logo from "./image1.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo  img-fluid" alt="logo" /> */}
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+import axios from "axios";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {}
+
+  function search(event) {
+    event.preventDefault();
+    alert(`searching for ${keyword} definition`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_us/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleKeyword(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeyword} />
+      </form>
+    </div>
+  );
+}
